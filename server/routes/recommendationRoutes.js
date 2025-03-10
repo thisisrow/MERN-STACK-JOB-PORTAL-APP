b08bad626b7e3111f2aea8fff943485358a9f5c0
thisisrow <prathameshmishra2020@gmail.com>
2025-03-10 11:27:09 +0530

    Update recommendationRoutes.js

--- server/routes/recommendationRoutes.js
+++ server/routes/recommendationRoutes.js
@@ -4,5 +4,6 @@ const recommendationController = require("../controllers/recommendationControlle
 const router = express.Router();
 
 router.get("/:userId", recommendationController.getRecommendations); // returns job recommendations for a user
+router.get('/:jobId', jobController.getUsersForJob);/ returns user recommendations for a job
 
 module.exports = router;
